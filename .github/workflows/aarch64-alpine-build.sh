496caea5ffde734c8ebf5ae4f7c0e1f0eeffcb97
Jonne Haß <me@jhass.eu>
2020-06-05 11:23:48 +0200

    enable drone for aarch64

---
+++ .github/workflows/aarch64-alpine-build.sh
@@ -0,0 +1,38 @@
+#!/bin/ash
+set -e
+
+if [ "$(id -u)" -eq 0 ]; then
+  #echo "::group::Install dependencies"
+  apk add build-base gc-dev llvm-dev libevent-dev pcre-dev zlib-dev libxml2-dev yaml-dev openssl-dev gmp-dev zlib-dev
+  #echo "::endgroup::"
+
+  #echo "::group::Switch to user"
+  adduser -D crystal
+  chown -R crystal .
+  chgrp -R crystal .
+  exec su crystal "$0" -- "$@"
+fi
+
+# echo "::endgroup::"
+
+#echo "::group::Build deps"
+make deps
+#echo "::endgroup::"
+
+#echo "::group::Link cross compiled compiler"
+mkdir -p .build
+cc crystal.o -o .build/crystal  -rdynamic src/llvm/ext/llvm_ext.o `/usr/bin/llvm-config --libs --system-libs --ldflags 2> /dev/null` -lstdc++ -lpcre -lm -lgc -lpthread src/ext/libcrystal.a -levent -lrt
+#echo "::endgroup::"
+
+#echo "::group::Rebuild Crystal"
+touch src/compiler/crystal.cr
+make
+#echo "::endgroup::"
+
+#echo "::group::Run stdlib specs"
+make std_spec
+#echo "::endgroup::"
+
+#echo "::group::Run compiler specs"
+make compiler_spec
+#echo "::endgroup::"
